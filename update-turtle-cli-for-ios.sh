7a8ab5d6aeaf888bbd5ed121748590b3341d3b9e
boonprakit chaikaew <boonprakitchaikaew@gmail.com>
2020-08-30 01:37:40 +0700

    create shell script for build script for circle ci

---
+++ update-turtle-cli-for-ios.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TAG=${1:-latest}
+
+export TURTLE_VERSION_NEW=`npm show turtle-cli@$TAG version`
+export APP_EXPO_SDK_VERSION="37.0.0"
+envsubst '${TURTLE_VERSION_NEW} ${APP_EXPO_SDK_VERSION}' < .circleci/ios.template.yml > .circleci/config.yml
+
+git add .circleci/config.yml README.md .travis.yml
+git commit -m "update to turtle-cli@$TURTLE_VERSION_NEW"
