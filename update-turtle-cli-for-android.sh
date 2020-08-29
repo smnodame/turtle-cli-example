95aac3df32eb273c8e36b56756f1e27c2bb7d4f9
boonprakit chaikaew <boonprakitchaikaew@gmail.com>
2020-08-30 02:46:02 +0700

    update to turtle-cli@0.17.2

--- update-turtle-cli-for-android.sh
+++ update-turtle-cli-for-android.sh
@@ -4,7 +4,7 @@ TAG=${1:-latest}
 
 export TURTLE_VERSION_NEW=`npm show turtle-cli@$TAG version`
 export APP_EXPO_SDK_VERSION="37.0.0"
-envsubst '${TURTLE_VERSION_NEW} ${APP_EXPO_SDK_VERSION}' < .circleci/android.template.yml > .circleci/config.yml
+envsubst '${TURTLE_VERSION_NEW} ${APP_EXPO_SDK_VERSION}' < .circleci/android.template.yml > .circleci/android.config.yml
 
 git add .
 git commit -m "update to turtle-cli@$TURTLE_VERSION_NEW"
