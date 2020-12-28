011c4e02e8d416e45ec87662edcbc4ebd084b626
boonprakit <boonprakit@proteus-tech.com>
2020-12-28 22:50:20 +0700

    fix wrong var name

--- commands/uploadFile.js
+++ commands/uploadFile.js
@@ -36,7 +36,7 @@ module.exports = {
 if (require.main === module) {
     const args = process.argv.slice(2)
     if (args.length >= 2) {
-        updateStatus(args[0], args[1])
+        uploadFile(args[0], args[1])
     } else {
         console.log('Status arg is not defined')
     }
