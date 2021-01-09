0f761a63ce3477d7d953130a8e89198ade07809b
shraredbots <shraredbots@gmail.com>
2021-01-09 18:27:51 +0000

    build

---
+++ src/messages/calendar/index.js
@@ -0,0 +1,123 @@
+import DateTimePicker from 'react-native-modal-datetime-picker'
+import React from 'react'
+import { View } from 'react-native'
+import _ from 'lodash'
+import generateMessage from '../../utils/generateMessage'
+import moment from 'moment'
+import { setVariables } from '../../modules/setVariables'
+
+export default class Calendar extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			visible: true,
+		}
+	}
+
+	componentDidMount = () => {}
+
+	onSend = (date) => {
+		const { question, answers, storeAnswers, onSend, variables } = this.props
+
+		this.setState(
+			{
+				visible: false,
+			},
+			() => {
+				const answer = {
+					[question.id]: {
+						answer: {
+							default: {
+								value: date,
+							},
+							datetime: {
+								value: moment(date).format('MMMM Do YYYY, h:mm:ss a'),
+							},
+							fulldate: {
+								value: moment(date).format('MMM Do YY'),
+							},
+							day: {
+								value: moment(date).format('dddd'),
+							},
+							minute: {
+								value: moment(date).minute(),
+							},
+							hour: {
+								value: moment(date).hour(),
+							},
+							date: {
+								value: moment(date).day(),
+							},
+							month: {
+								value: moment(date).month() + 1,
+							},
+							year: {
+								value: moment(date).year(),
+							},
+							created_at: {
+								value: new Date(),
+							},
+						},
+						mode: question.mode,
+					},
+				}
+
+				setVariables(question.id, variables, answer)
+				
+				// store answers in store
+				storeAnswers(
+					{
+						...answers,
+						...answer,
+					},
+					answer
+				)
+
+				// generate answer and replace expression
+				const message = generateMessage(
+					question.answer || this.getDefaultFormat(date),
+					answers,
+					answer
+				)
+
+				onSend(
+					{
+						text: message,
+					},
+					_.get(question, 'trigger.right')
+				)
+			}
+		)
+	}
+
+	getDefaultFormat = (date) => {
+		const { question } = this.props
+		const mode = _.get(question, 'input.calendar.mode', 'datetime')
+		if (mode === 'datetime') {
+			return moment(date).format('MMMM Do YYYY, h:mm:ss a')
+		} else if (mode === 'time') {
+			return moment(date).format('h:mm:ss a')
+		} else if (mode === 'date') {
+			return moment(date).format('MMMM Do YYYY')
+		}
+	}
+
+	render() {
+		const { question } = this.props
+
+		const mode = _.get(question, 'input.calendar.mode', 'datetime')
+		const buttonText = _.get(question, 'input.button.text', 'Confirm')
+
+		return (
+			<View>
+				<DateTimePicker
+					isVisible={this.state.visible}
+					onConfirm={this.onSend}
+					onCancel={() => {}}
+					mode={mode}
+					confirmTextIOS={buttonText}
+				/>
+			</View>
+		)
+	}
+}
