2c20373b7e2fab47f5cd41fb65054458266927ef
shraredbots <shraredbots@gmail.com>
2021-01-09 07:34:50 +0000

    build

---
+++ commands/prepareAndroidEnv.js
@@ -0,0 +1,28 @@
+const config = require('../config.json')
+const us = require('./updateStatus')
+const util = require('util')
+const fs = require('fs')
+const os = require('os')
+
+const appendFileAsync = util.promisify(fs.appendFile)
+
+async function androidBuild() {
+    try {
+        await us.updateStatus('BUILDING')
+
+        const setENVScriptPath = '/home/circleci/expo-project/setEnv.sh'
+        await appendFileAsync(setENVScriptPath, `echo 'export EXPO_ANDROID_KEYSTORE_ALIAS=${config.EXPO_ANDROID_KEYSTORE_ALIAS}' >> $BASH_ENV`+ os.EOL)
+        await appendFileAsync(setENVScriptPath, `echo 'export EXPO_ENV_FROM=${config.EXPO_ENV_FROM}' >> $BASH_ENV`+ os.EOL)
+        await appendFileAsync(setENVScriptPath, `echo 'export EXPO_ANDROID_KEYSTORE_PASSWORD=${config.EXPO_ANDROID_KEYSTORE_PASSWORD}' >> $BASH_ENV`+ os.EOL)
+        await appendFileAsync(setENVScriptPath, `echo 'export EXPO_ANDROID_KEY_PASSWORD=${config.EXPO_ANDROID_KEY_PASSWORD}' >> $BASH_ENV`+ os.EOL)
+        await appendFileAsync(setENVScriptPath, `echo 'export EXPO_ANDROID_KEYSTORE_BASE64=${config.EXPO_ANDROID_KEYSTORE_BASE64}' >> $BASH_ENV`+ os.EOL)
+        await appendFileAsync(setENVScriptPath, `source $BASH_ENV`+ os.EOL)
+    } catch (err) {
+        await us.updateStatus('FAILED', err.message)
+        process.exitCode = 1
+    }
+}
+
+if (require.main === module) {
+    androidBuild()
+}
---
+++ src/redux/sagas/onSend.js
@@ -0,0 +1,111 @@
+import * as apis from '../apis'
+import * as selectors from '../selectors'
+
+import { call, put, select, take } from 'redux-saga/effects'
+
+import EventLog from '../../modules/eventlog'
+import actions from '../actions'
+import conf from '../../conf'
+import getMessageDuration from '../../utils/getMessageDuration'
+import { store } from '../index'
+import types from '../types'
+
+function* onSendSaga() {
+	while (true) {
+		const {
+			payload: { data, trigger },
+		} = yield take(types.ON_SEND)
+		const id = new Date().getTime()
+		const question = yield select(selectors.getCurrentQuestion)
+		const message = {
+			_id: id,
+			id: id,
+			createdAt: new Date(),
+			user: {
+				_id: 1,
+				id: 1,
+			},
+			question: question,
+			loaded: true,
+			...data,
+		}
+
+		try {
+			// clear mode field for bubble question
+			if (question.mode === 'APPROVE') {
+				delete question.mode
+			}
+
+			const oldMessages = yield select(selectors.getMessages)
+
+			// store latest question in store
+			yield put(actions.lastQuestionAnswer(question))
+
+			const loadedMessages = Object.values(oldMessages).reduce((o, n) => {
+				return {
+					...o,
+					[n.id]: {
+						...n,
+						loaded: true,
+					},
+				}
+			}, {})
+
+			const newMessages = {
+				[message.id]: {
+					...message,
+				},
+				...loadedMessages,
+			}
+
+			// add mesage to chat board
+			yield put(actions.messages(newMessages))
+
+			// store the next question
+			yield put(actions.nextMessage(trigger))
+
+			// clear current question
+			yield put(actions.currentQuestion({}))
+
+			// add message to admin (pusher server)
+			if (conf.MODE === 'MOBILE' || conf.MODE === 'FULL-CHATBOT') {
+				const { room_id: roomId } = yield select(selectors.getResponse)
+				yield call(apis.sendMessage, message, roomId)
+			}
+
+			if (trigger) {
+				// run next step after x seconds
+				store.dispatch(
+					actions.onAddTimeout(
+						setTimeout(() => {
+							store.dispatch(actions.runMessage(trigger))
+						}, getMessageDuration())
+					)
+				)
+			} else {
+				store.dispatch(
+					actions.onAddTimeout(
+						setTimeout(() => {
+							store.dispatch(actions.done())
+						}, 500)
+					)
+				)
+			}
+		} catch (err) {
+			const failedMessages = yield select(selectors.getFailedMessages)
+			EventLog.error('send message', {
+				message: err.message
+			})
+			yield put(
+				actions.failedMessages({
+					...failedMessages,
+					[id]: {
+						...message,
+					},
+				})
+			)
+		}
+	}
+}
+
+export default onSendSaga
