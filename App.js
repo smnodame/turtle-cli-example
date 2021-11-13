911cb535abe0b3b17c5d36bba9b85df4a03a2cc2
boonprakit chaikaew <boonprakitchaikaew@gmail.com>
2021-11-13 19:37:43 +0700

    test

--- App.js
+++ App.js
@@ -1,3 +1,19 @@
-import HybridApp from './src/app'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default HybridApp
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>turtle-cli-example</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
