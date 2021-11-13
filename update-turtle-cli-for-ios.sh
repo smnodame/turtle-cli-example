6f6b750adf3073d5694dc30fb4011ff72d19f2f1
boonprakit chaikaew <boonprakitchaikaew@gmail.com>
2021-11-13 11:58:34 +0700

    test

--- update-turtle-cli-for-ios.sh
+++ update-turtle-cli-for-ios.sh
@@ -3,7 +3,7 @@
 TAG=${1:-latest}
 
 export TURTLE_VERSION_NEW=`npm show turtle-cli@$TAG version`
-export APP_EXPO_SDK_VERSION="37.0.0"
+export APP_EXPO_SDK_VERSION="39.0.0"
 envsubst '${TURTLE_VERSION_NEW} ${APP_EXPO_SDK_VERSION}' < .circleci/ios.template.yml > .circleci/ios.config.yml
 
 git add .
